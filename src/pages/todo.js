e742806fbdbdcb6b11f7eb0b3043d635f2ea09ea
doganuk <dogan.uk@outlook.com>
2023-02-25 00:00:26 +0000

    searchBox

--- src/pages/todo.js
+++ src/pages/todo.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Todo(){
 
-    const todos =['todo1','todo2','todo3']
+    const [search, SetSearch] = useState('');
+    const [todos, SetTodos] = useState([]);
+
+    const searchFunction = (value) => {
+        console.log(value);
+    }
+
+    useEffect(()=>{
+        SetTodos(['todo1','todo2','todo3'].filter(i=> i.includes(search)))
+    },[search])
 
     return(
+        <main id='main' className="test">
+        <label htmlFor="search" onClick={searchFunction}>Arama</label>
+        <input type="text" id="search" onChange = {e => SetSearch(e.target.value)}/>
         <ul>
             {
                 todos.map(todo => 
@@ -14,5 +26,6 @@ export default function Todo(){
                 )
             }
         </ul>
+        </main>
     )
 }
