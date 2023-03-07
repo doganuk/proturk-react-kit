7882905afc898c5c4f0a6aff4f3c72fabd86df5e
doganuk <dogan.uk@outlook.com>
2023-03-07 16:11:49 +0000

    Parenttan chilldaki state degerini degistirmek: * burada prop olarak aldigiactiveTab degisince , onu dinliyor ve o degistiginde icerdeki active 'i setActive ile set ediyor. * Ayrica kendisininde onClick durumunda bu active 'i set etme ozelligi var ==> bu calisiyor ama mesela kendisi active i 1 yapti, parentta active 2 ydi en son diyelim buttona tekrar basinca onu 2 set etmiyor cunku parent render olmuyor ama textboxda 1 yapip tekrar buttona basinca parent tekrar render oluyor ve parenttaki deger ile icerdeki active i set ediyor! burdaki esas problem truth of source u bozduk.

--- src/pages/Tab.js
+++ src/pages/Tab.js
@@ -1,14 +1,36 @@
-function Tab({ children }) {
 
+import {useEffect, useState} from "react"  
+
+function Tab({ children, activeTab }) {
+    const [active, setActive] = useState(activeTab);
+    //console.log('render oldu')
+    useEffect(()=>
+        setActive(activeTab)
+    ,[activeTab])
+
+    useEffect(()=>
+        console.log(active)
+    ,[active])
 
     return (
+        <>
         <nav>
-               {children.map((item,index)=> <p key={index}>{item.props.title}</p>)}
+            {children.map((item, index) =>
+                <div key={index} onClick={() => setActive(index)} className={active == index ? 'blue' : 'white'}>
+                    {item}
+                </div>)
+            }
+            
+            
+               {/* {children.map((item,index)=> <div key={index}>{item.props.title}</div>)}
                <hr/>
-               {children.map((item,index)=> <p key={index}>{item.props.children}</p>)}
-               <hr/>
-               {children.map((item,index)=> <p key={index}>{item}</p>)}
+               {children.map((item,index)=> <div key={index}>{item.props.children}</div>)}
+               <hr/> */}
+
         </nav>
+
+        {children[active]}  
+        </>
     );
 }
 
--- src/pages/customTab.js
+++ src/pages/customTab.js
@@ -1,15 +1,22 @@
+import { useState,useRef } from 'react'
 import Tab from './Tab'
 
 export default function CustomTab(){
 
+    const [selectedTab, setselectedTab] =useState(1);
+    const regInput = useRef(1);
+
 return (
 
     <div style ={{padding:20, backgroundColor:"yellow"}}>
-        <Tab>
+        <Tab activeTab= {selectedTab} >
             <Tab.Panel title="1.Tab">Men</Tab.Panel>
             <Tab.Panel title="2. Tab">Women</Tab.Panel>
             <Tab.Panel>Unisex</Tab.Panel>    
         </Tab>        
+        <button onClick={()=> setselectedTab(regInput.current.value)}>setActive 2</button>
+         enter a tab number<input ref={regInput} />
+         
     </div>
 )
 
